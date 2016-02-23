d173bce201cd689cab6fd2c8df16e76831edb4c3
WilliamCStark <williamstarkemail@gmail.com>
2016-02-23 11:47:50 +1100

    ListLooping Lesson

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,22 @@
-my_list = [1,2,3,5]
+my_list = [0, 0, 0, 0, 0, 0]
+elementToChange = ""
+indexToAddTo = 0
 
+elementToChange = input("Please enter anything you like: ")
 try:
-    x = my_list[5]
-except IndexError:
-    print("Out of Range")
+    elementToChange = int(elementToChange)
+except TypeError:
+    print("Error")
+
+CorrectInput = False
+while not CorrectInput:
+    try:
+        indexToAddTo = int(input("Choose where to add this in my_list that has " + str(len(my_list)) + " elements: "))
+        my_list[indexToAddTo] = elementToChange
+        CorrectInput = True
+    except TypeError:
+        print("Please enter an integer.")
+    except IndexError:
+        print("Please choose an element that lies in the list.")
+
+print(str(my_list(indexToAddTo)))
--- listlooping.py
+++ listlooping.py
@@ -1,1 +1,20 @@
-# write a program to read in 10 values from the keyboard and append them to a list.
+# Write a program to read in 10 values from the keyboard and append them to a list
+
+def sumIsGreaterThan(my_list, upper_bound):
+    counter = 0
+    for i in my_list:
+        counter += i
+    return counter > upper_bound
+
+
+stuff = []
+
+while not sumIsGreaterThan(stuff, 100):
+    val = input("Enter a value: ")
+    try:
+        val = int(val)
+        stuff.append(val)
+    except TypeError:
+        val = input("Please enter an integer: ")
+
+print(stuff)
